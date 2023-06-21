928351daccd11d0099617f4111dfc1139d81c4f4
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-06-21 15:04:00 +0000

    Variables in python

---
+++ Days.py
@@ -0,0 +1,8 @@
+#  variables
+
+calculate_to_units = 24 
+name_of_units = "hours"
+
+print(f"20 days are have {20 * calculate_to_units} {name_of_units}")
+
+print(f"35 days are have {35 * calculate_to_units} {name_of_units}")
