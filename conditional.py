1412f0ae6cdde39229757c11e2771b0598e4821d
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-07-27 11:15:44 +0000

    correcting

--- conditional.py
+++ conditional.py
@@ -2,6 +2,7 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 
+
 def days_to_units(num_of_days):
     condition_check = num_of_days > 0
     print(type(condition_check))
@@ -13,16 +14,17 @@ def days_to_units(num_of_days):
     # else:
     #     return "you entered a negative value , no calculation for you!"
 
+user_input = input("hey user enter a number and will convert it in hours -\n")
+
+
 def excecute_and_validate():
     if user_input.isdigit():
-      user_input_number = int(user_input)
-      calculated_value = days_to_units(user_input_number)
-      print(calculated_value)
+        user_input_number = int(user_input)
+        calculated_value = days_to_units(user_input_number)
+        print(calculated_value)
     else:
         print("you entered a non-integer value , plz provide a integer value")
 
-
-        user_input = input("hey user enter a number -\n")
-   
+excecute_and_validate()
+        
 # print(type("this should be a type of string"))
- 
